e1412a8aeccae4a0213c552d03c517e57fda3d1c
Francois Berenger <unixjunkie@sdf.org>
2020-06-04 16:25:58 +0900

    more commands in the R install file

--- files/install_keras.r
+++ files/install_keras.r
@@ -3,3 +3,6 @@ if (!require(pkg, character.only = TRUE)) {
   install.packages(pkg, dependencies = TRUE,
                    repos='http://cran.r-project.org')
 }
+library(keras)
+# CPU version
+install_keras(method = "conda", tensorflow = "cpu")
