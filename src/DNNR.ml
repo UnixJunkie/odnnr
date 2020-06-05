c78a5794ce92f7df90c6bf8bc2f03b6529e34dd5
Francois Berenger <unixjunkie@sdf.org>
2020-06-05 14:41:15 +0900

    renamed:    src/ODNNR.ml -> src/DNNR.ml

--- src/DNNR.ml
+++ src/DNNR.ml
@@ -13,3 +13,11 @@ type optimizer = SGD (* Stochastic Gradient Descent *)
  * and https://keras.io/api/metrics/ *)
 type loss_or_metric = MSE (* Mean Squared Error *)
                     | MAE (* Mean Absolute Error *)
+
+(* constrain the range the output can take *)
+type activation_function = None (* value in any range can be output *)
+                         | Relu (* classic choice for hidden layers *)
+                         | Sigmoid (* for probabilistic output *)
+
+type hidden_layer = { units: int;
+                      activation: activation_function }
