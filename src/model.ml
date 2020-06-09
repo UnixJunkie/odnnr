6b7e8bc184162d63c02cfc44a19dfa43ac8159ed
Francois Berenger <unixjunkie@sdf.org>
2020-06-09 16:04:14 +0900

    Model: added --early-stop

--- src/model.ml
+++ src/model.ml
@@ -88,10 +88,10 @@ let early_stop verbose optimizer loss hidden_layers
   let rec loop best_R2 best_epochs curr_epochs nb_fails =
     if curr_epochs >= max_epochs then
       (Log.error "Model.early_stop: max epochs reached";
-       (best_R2, best_epochs))
+       (model_fn, best_R2, best_epochs))
     else if nb_fails = patience then
       (Log.info "Model.early_stop: patience reached";
-       (best_R2, best_epochs))
+       (model_fn, best_R2, best_epochs))
     else
       begin
         DNNR.early_stop_continue verbose delta_epochs model_fn;
@@ -104,7 +104,7 @@ let early_stop verbose optimizer loss hidden_layers
         else (* curr_R2 <= best_R2 *)
           loop best_R2 best_epochs (curr_epochs + delta_epochs) (nb_fails + 1)
       end in
-  (model_fn, loop init_R2 delta_epochs delta_epochs 0)
+  loop init_R2 delta_epochs delta_epochs 0
 
 let main () =
   Log.(set_log_level DEBUG);
@@ -120,8 +120,9 @@ let main () =
                [-p <float>]: train portion; default=%f\n  \
                [--seed <int>]: RNG seed\n  \
                [--test <test.txt>]: test set\n  \
-               [--epochs <int>]: optimal number of training epochs\n  \
+               [--epochs <int>]: optimal/max number of training epochs\n  \
                [-np <int>]: max CPU cores\n  \
+               [--early-stop]: early stopping epochs scan\n  \
                [--NxCV <int>]: number of folds of cross validation\n  \
                [-s|--save <filename>]: save model to file\n  \
                [-l|--load <filename>]: restore model from file\n  \
@@ -139,6 +140,7 @@ let main () =
       exit 1
     end;
   let verbose = CLI.get_set_bool ["-v"] args in
+  let epochs_scan = CLI.get_set_bool ["--early-stop"] args in
   let ncores = CLI.get_int_def ["-np";"--nprocs"] args 1 in
   let seed = match CLI.get_int_opt ["-s";"--seed"] args with
     | Some s -> s (* reproducible *)
@@ -191,8 +193,15 @@ let main () =
         (* train/test split *)
         Log.info "shuffle -> train/test split (p=%.2f)" train_portion;
         let train_fn, test_fn = shuffle_then_cut seed train_portion train_fn' in
-        ignore(train_test verbose no_plot
-                 optimizer loss hidden_layers nb_epochs train_fn test_fn)
+        if epochs_scan then
+          let model_fn, best_R2, best_epochs =
+            early_stop verbose optimizer loss hidden_layers
+              nb_epochs 1 5 train_fn test_fn in
+          Log.info "model_fn: %s best_R2: %.3f epochs: %d"
+            model_fn best_R2 best_epochs
+        else
+          ignore(train_test verbose no_plot
+                   optimizer loss hidden_layers nb_epochs train_fn test_fn)
       end
 
 let () = main ()
