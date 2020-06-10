32d8c2200a0956549b780f2fa987673dcfdb0c9f
Francois Berenger <unixjunkie@sdf.org>
2020-06-10 17:48:04 +0900

    commands

--- commands.sh
+++ commands.sh
@@ -2,17 +2,15 @@
 
 set -x
 
-# find best model using early stopping
-for l in 32 64 96 128; do
-    # hidden layers: 1 to 5
-    echo odnnr_model --no-plot --train data/Boston_regr_train.csv \
-         --epochs 100 --early-stop --arch $l
-    echo odnnr_model --no-plot --train data/Boston_regr_train.csv \
-         --epochs 100 --early-stop --arch $l/$l
-    echo odnnr_model --no-plot --train data/Boston_regr_train.csv \
-         --epochs 100 --early-stop --arch $l/$l/$l
-    echo odnnr_model --no-plot --train data/Boston_regr_train.csv \
-         --epochs 100 --early-stop --arch $l/$l/$l/$l
-    echo odnnr_model --no-plot --train data/Boston_regr_train.csv \
-         --epochs 100 --early-stop --arch $l/$l/$l/$l/$l
+# find best model architecture using early stopping
+for l in 64 128 256 512 1024 2048; do
+    # hidden layers: 1 to 4
+    echo odnnr_model --no-plot --train data/chembl1868_good.pFP \
+         --NxCV 5 --epochs 100 --early-stop --arch $l
+    echo odnnr_model --no-plot --train data/chembl1868_good.pFP \
+         --NxCV 5 --epochs 100 --early-stop --arch $l/$l
+    echo odnnr_model --no-plot --train data/chembl1868_good.pFP \
+         --NxCV 5 --epochs 100 --early-stop --arch $l/$l/$l
+    echo odnnr_model --no-plot --train data/chembl1868_good.pFP \
+         --NxCV 5 --epochs 100 --early-stop --arch $l/$l/$l/$l
 done
