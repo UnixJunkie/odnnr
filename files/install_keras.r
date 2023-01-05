e417005b176fc2286d4359625fbc93b54fb87141
Francois Berenger <unixjunkie@sdf.org>
2023-01-05 12:41:37 +0900

    files/install_keras.r: updated

--- files/install_keras.r
+++ files/install_keras.r
@@ -1,8 +1,8 @@
+# for keras and tensorflow, let's assume the user has installed them in
+# user-space as a prerequisite using 'pip3 install tensorflow keras'
+# here, we just test that the keras library can be loaded in R
 pkg <- 'keras'
 if (!require(pkg, character.only = TRUE)) {
   install.packages(pkg, dependencies = TRUE, repos='http://cran.r-project.org')
 }
 library(keras)
-# CPU version; GPU version requires '= "gpu"' and installing all the
-# required dependencies (like Nvidia proprietary packages) is a mess
-install_keras(method = "conda", tensorflow = "cpu")
